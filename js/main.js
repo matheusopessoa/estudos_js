0c7a2d61e30b3e18fb9d6a6f215081a7f7e9d2be
unknown <matheuspessoa.tech@gmail.com>
2023-11-11 10:27:23 -0300

    Anotacoes 11/11

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const nome = "Matheus"
+
+console.log(document.querySelector(".container"))
+let user_name = window.prompt("Insira o seu nome")
+console.log(user_name)
+
+var confirmacao = window.confirm("Confirma")
+console.log(confirmacao)
