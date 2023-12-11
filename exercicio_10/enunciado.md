173e7c15e0f512d432ad21527f5d2fa536cdb3f4
Matheus Pessoa <matheuspessoa.tech@gmail.com>
2023-12-11 20:24:40 -0300

    Exercicio final

--- exercicio_10/enunciado.md
+++ exercicio_10/enunciado.md
@@ -10,13 +10,13 @@ Inscrever um candidato em uma vaga
 Excluir uma vaga
 Sair
 
-A opção de listar as vagas deve mostrar o índice, o nome e a quantidade de candidatos inscritos de todas as vagas.
+1- A opção de listar as vagas deve mostrar o índice, o nome e a quantidade de candidatos inscritos de todas as vagas.
 
-A opção de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite, e também deve pedir que o usuário confirme as informações antes de salvá-las.
+2- A opção de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite, e também deve pedir que o usuário confirme as informações antes de salvá-las.
 
-A opção de visualizar uma vaga deve pedir o índice da vaga e mostrar todas as informações dela: índice, nome, descrição, data limite, quantidade de candidatos e o nome dos candidatos.
+3- A opção de visualizar uma vaga deve pedir o índice da vaga e mostrar todas as informações dela: índice, nome, descrição, data limite, quantidade de candidatos e o nome dos candidatos.
 
-A opção de inscrever um candidato em uma vaga de pedir o nome do candidato, o índice da vaga e então uma confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+4- A opção de inscrever um candidato em uma vaga de pedir o nome do candidato, o índice da vaga e então uma confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
 
 A opção de excluir uma vaga deve pedir o índice da vaga, mostrar suas informações e pedir que o usuário confirme a exclusão da vaga antes de realmente exclui-la.
 
--- exercicio_10/sistema_vagas.js
+++ exercicio_10/sistema_vagas.js
@@ -2,12 +2,13 @@ const vagas = []
 
 function menu() {
     const opcao = prompt('Escolha uma opção:\n' +
-    '1. Listar Vagas\n' + '2. Inserir Vaga\n' + '3. Visualizar Vaga')
+    '1. Listar Vagas\n' + '2. Inserir Vaga\n' + '3. Visualizar Vaga\n' + '4. Inserir candidato\n' +
+    '5. Excluir vaga\n' + '6. Sair')
     return opcao
 }
 
 function listarVagas() {
-    for (i = 0; i < length(vagas); i++) {
+    for (i = 0; i < vagas.length; i++) {
         let vagaAtual = vagas[i]
         let nome = vagaAtual.nome
         let candidatos = vagaAtual.candidatos
@@ -31,16 +32,34 @@ function inserirVaga() {
 
 function visualizarVaga() {
     let indiceVisto = parseFloat(prompt('Digite o indice da vaga'))
-    let vagaAtual = vagas[indiceVisto]
-    let nome = vagas[indiceVisto].nome
-    let candidatos = vagaAtual.candidatos.length
+
+    let vagaAtual = vagas[indiceVisto];
+    let nome = vagas[indiceVisto].nome;
+    let candidatos = vagaAtual.candidatos;
+    if (vagas.length >= indiceVisto) {
+        alert('Indice: ' + indiceVisto + '\n' +
+            'Nome da vaga: ' + nome + '\n' +
+            'Quantidade de candidatos: ' + candidatos + '\n' +
+            'Nome dos candidatos: ' + candidatos.length + '\n')
+        }
+    else {
+        alert('Index inválido')
+    }
+}
+
+function inserirCandidato() {
+    let indeceDaVaga = parseFloat(prompt('Digite o indice da vaga'))
+    let nomeCand = prompt('Digite o nome do candidato')
+    let vagaDoCandidato = vagas[indeceDaVaga]
+    let listaCandidatosDaVaga = vagaDoCandidato.candidatos
+
+    listaCandidatosDaVaga.push(nomeCand)
+}
+
+function excluirVaga() {
+    let indiceDaVaga = parseFloat(prompt('Digite o índice da vaga'))
     
-    const nomeCandidatos = candidatos.map(function(vaga) {
-        return vaga.nomeCandidato})
-    alert('Indice: ' + indiceVisto + '\n' +
-          'Nome da vaga: ' + nome + '\n' +
-          'Quantidade de candidatos: ' + candidatos + '\n' +
-          'Nome dos candidatos: ' + nomeCandidatos + '\n')
+    vagas.splice(indiceDaVaga, 1)
 }
 
 function executar() {
@@ -61,8 +80,15 @@ function executar() {
             case '3':
                 visualizarVaga()
                 break
+            case '4':
+                inserirCandidato()
+                break
+            case '5':
+                excluirVaga()
+                break
             case '6':
                 alert('Você escolheu sair...')
+                break
             default:
                 alert('OPCAO INVÁLIDA')
         }
